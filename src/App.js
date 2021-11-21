4feeb473c29230a5190dc9e5cea5a331a6a77c34
unknown <gd_dada@esi.dz>
2021-11-21 01:04:41 +0100

    show the first 20 page

--- src/App.js
+++ src/App.js
@@ -2,23 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 import User from './components/User';
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
   const [users, setUsers] = useState([])
+  console.log("da");
   useEffect(() => {
-    async function name(params) {
-      
+    async function get_all_users(params) {
+      try {
+        const request = await axios.get('https://randomuser.me/api/?page=1&results=20')
+        console.log('😁😁', request.data.results);
+        setUsers(request.data.results)
+      } catch (error) {
+        console.log('error on loading data❌❌❌');
+      }
     }
+
+    get_all_users()
+
   }, [])
   return (
     <div className="app">
       <div className='main__leftside'>a</div>
       <div className='main__users'>
-        <User/>
-        <User/>
-        <User/>
-        <User/>
-        <User/>
+        {users.map((user, index)=>(
+          <User key={index} user={user} />
+        ))}
       </div>
       <div className='main__rightside'>a</div>
     </div>
--- src/components/User.js
+++ src/components/User.js
@@ -1,18 +1,19 @@
 import './user.css'
 
-const User = () => {
+const User = ({user}) => {
     return (
         <div className='user' >
             <div className='user__infos' >
-                <h3 className='user__infos__name' >Name : name</h3>
-                <h3 className='user__infos__username'>Username : username</h3>
-                <h3 className='user__infos__email'>User email : email</h3>
-                <h3 className='user__infos__phone'>User phone : phone</h3>
+                <h3 className='user__infos__name' >Name : {user.name.first} {user.name.last}</h3>
+                <h3 className='user__infos__email'>User email : {user.email}</h3>
+                <h3 className='user__infos__phone'>User phone : {user.phone}</h3>
             </div>
 
             <div className='user__image' >
-                <img alt='user__photo' src='https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/768px-User_icon_2.svg.png' />
+                <img alt='user__photo' src={user.picture.large} />
+                <button>More infos</button>
             </div>
+
         </div>
     )
 }
